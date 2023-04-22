a9115306cc9288343a4b2e59c7173ae3d6cec05e
AlexInABox <main@alexinabox.de>
2023-04-22 22:18:10 +0200

    new style + bakcround

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-const fs = require('fs')
+var data;
+main();
+setInterval(function () { main() }, 5000);
 
-var hrateInterval = setInterval(function () {
-    var hrate = fs.readFileSync('custom-hds/hrate.txt', 'utf8');
-    document.getElementById("heartRate").innerHTML = hrate;
-}, 1000);
+async function main() {
+    data = await fetchData();
+    await patchContent();
+}
+
+async function fetchData() {
+    console.log("getting data");
+    return new Promise((obj) => {
+        fetch('./custom-hds/healthData.json')
+            .then(res => res.json())
+            .then(data => {
+                return obj(data)
+            })
+    })
+}
+
+async function patchContent() {
+    console.log("patching content");
+    document.getElementById("heartRate").innerHTML = data.heartRate;
+    updateOxygenSaturationBar(data.oxygenSaturation);
+    document.getElementById("speed").innerHTML = data.speed;
+}
+
+function updateOxygenSaturationBar(value) {
+
+    value = value * 100;
+    value = Math.round(value);
+    //animate the transition to the new value
+    var elem = document.getElementById("oxygenSaturation-bar");
+    var width = document.getElementById("oxygenSaturation-bar").style.width;
+    var width = width.substring(0, width.length - 1);
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width > value) {
+            width--;
+            elem.style.width = (width) + '%';
+            document.getElementById("oxygenSaturation-bar").innerHTML = elem.style.width;
+        }
+        else if (width < value) {
+            width++;
+            elem.style.width = (width) + '%';
+            document.getElementById("oxygenSaturation-bar").innerHTML = elem.style.width;
+        }
+        else {
+            clearInterval(id);
+        }
+
+    }
+}
